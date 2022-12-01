f97a95e4bcba4286ebb71314d665f9a1989e849b
paulr093 <paulrichan@icloud.com>
2022-12-01 08:48:56 -0700

    Day 1 finished - python

---
+++ Day1/day1.py
@@ -0,0 +1,23 @@
+with open("./day1input.txt", "r") as f:
+    allNums = [num.rstrip() for num in f]
+
+    arrayOfSections = []
+    secArray = []
+
+    for ele in allNums:
+        if ele == "":
+            arrayOfSections.append(secArray)
+            secArray = []
+        else:
+            secArray.append(int(ele))
+
+    sumOfSection = []
+
+    for sec in arrayOfSections:
+        sumOfSection.append(sum(sec))
+
+    sortedTop3 = sorted(sumOfSection, reverse=True)[:3]
+
+    print("Your max number is " + str(max(sumOfSection)))
+
+    print(f"Here is your top 3 total: {sum(sortedTop3)}")
